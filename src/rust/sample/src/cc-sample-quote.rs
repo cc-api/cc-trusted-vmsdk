f6ae5be0b04f195fbbb057408a8bf3a6788427b3
hairongchen <hairong.chen@intel.com>
2024-03-01 14:34:01 +0800

    Rust SDK: enhance to support get TDX quote via vsock Signed-off-by: Hairong Chen hairong.chen@intel.com

--- src/rust/sample/src/cc-sample-quote.rs
+++ src/rust/sample/src/cc-sample-quote.rs
@@ -29,9 +29,11 @@ fn main() {
         }
     };
 
+    info!("cc report size {}", report.cc_report.len());
+
     // dump the cc report with API "dump_cc_report"
-    //info!("call cc trusted API [dump_cc_report] to dump cc report!");
-    API::dump_cc_report(&report.cc_report);
+    // info!("call cc trusted API [dump_cc_report] to dump cc report!");
+    // API::dump_cc_report(&report.cc_report);
 
     // parse the cc report with API "parse_cc_report"
     if report.cc_type == TeeType::TDX {
